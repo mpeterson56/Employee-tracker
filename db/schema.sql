7afa3d34dd04859f9cf03e1fdba5b736cd892a96
Mike Peterson <mikemikemike3390@yahoo.com>
2022-06-09 19:17:26 -0700

    setup schema outline , needs to be fixed

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,24 @@
 DROP DATABASE IF EXISTS  employee_tracker;
 
 CREATE DATABASE  employee_tracker;
+
+CREATE TABLE department (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+  
+);
+
+CREATE TABLE  role  (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+);
+
+CREATE TABLE  employee  (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name DECIMAL NOT NULL,
+  role_id INTEGER foreign key NOT NULL,
+manager_id integer foreign key NOT NULL
+);
