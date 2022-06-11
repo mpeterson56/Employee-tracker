c3abb0b8ad87ffd07e0d9db7d7be887cb2d640ae
Mike Peterson <mikemikemike3390@yahoo.com>
2022-06-11 12:22:36 -0700

    fixed tables in my db , moving on to routes

--- Routes/index.js
+++ Routes/index.js
@@ -1,11 +1,3 @@
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
 
-const apiRoutes = require('./api');
-
-router.use('/api', apiRoutes);
-
-router.use((req, res) => {
-  res.status(404).end();
-});
-
-module.exports = router;
--- db/schema.sql
+++ db/schema.sql
@@ -2,23 +2,27 @@ DROP DATABASE IF EXISTS  employee_tracker;
 
 CREATE DATABASE  employee_tracker;
 
+USE employee_tracker;
+
 CREATE TABLE department (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL,
-  
+  department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE  role  (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  id INTEGER AUTO_INCREMENT PRIMARY KEY ,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
-  department_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+   department_id INTEGER ,
+  FOREIGN KEY (department_id) REFERENCES department(id) on delete set null
+
 );
 
 CREATE TABLE  employee  (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
   first_name VARCHAR(30) NOT NULL,
-  last_name DECIMAL NOT NULL,
-  role_id INTEGER foreign key NOT NULL,
-manager_id integer foreign key NOT NULL
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER ,
+manager_id Integer ,
+FOREIGN KEY (role_id) REFERENCES role(id) on delete set null
 );
